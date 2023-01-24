f4c7a9d0571342f1eace791285b51f19140d46f0
Richardson Jean <jeanr211@mail.broward.edu>
2023-01-24 05:04:06 -0500

    updated the javascript file.

--- js/script.js
+++ js/script.js
@@ -151,7 +151,7 @@ const nav = document.querySelector(".nav"),
             }
         }
       }
-      /*document.querySelector(".hire-me").addEventListener("click", function(){
+      document.querySelector(".hire-me").addEventListener("click", function(){
           const sectionIndex = this.getAttribute("data-section-index");
           //console.log(sectionIndex);
           showSection(this);

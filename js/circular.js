ff3d8d2dc2989b402b07ac30cf6ff54753a84af7
Richardson Jean <jeanr211@mail.broward.edu>
2023-01-24 04:37:06 -0500

    New update

--- js/circular.js
+++ js/circular.js
@@ -3,6 +3,9 @@ $( document ).ready(function() {
 		$("#mega-wrapper").toggleClass("sb_showing");
     $("#page").toggleClass("mymargin")
 	});//End
+  $(".mylis").click(function () {
+    $("#mega-wrapper").removeClass("sb_showing");
+  });
     $('#share').on('click', function(){
     $('#iconwrapper li').toggleClass('slideout');
   });
